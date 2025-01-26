4add1855c9f7ac883584cfe09ea00e470e1629ee
StevieJSmith <smithjstevie@gmail.com>
2025-01-26 16:27:57 +0000

    Report updated

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 def main():
     with open("books/frankenstein.txt") as f:
+
         file_contents = f.read()
+
         print("==============================================")
         print("--- Begin report of books/frankenstein.txt ---")
         print("==============================================")
+
         print(f"{count_words(file_contents)} words found in the book")
+        
         print("------")
-        count_characters(file_contents)
+
+        character_dict = count_characters(file_contents)
+        for character in character_dict:
+            if ord(character) >= 97 and ord(character) <= 122: # could use is .isalpha() instead for same result! 
+                print(f"The '{character}' character was found {character_dict[character]} times")
+
         print("------")
         print("--- End report ---")
+        print("------")
 
 def count_words(text):
     return len(text.split())
@@ -22,10 +32,8 @@ def count_characters(text):
             character_dict[letter] = 1
         elif letter in character_dict:
             character_dict[letter] += 1
-    
-    for character in character_dict:
-        if ord(character) >= 97 and ord(character) <= 122:
-            print(f"The '{character}' character was found {character_dict[character]} times")
+
+    return character_dict
 
 
 main()
