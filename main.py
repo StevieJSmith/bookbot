32d19b2ba97804832f5045c7c03401f7ede2a88c
StevieJSmith <smithjstevie@gmail.com>
2025-01-26 16:07:41 +0000

    Count character function added

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         count_words(file_contents)
+        print("------")
+        count_characters(file_contents)
 
 def count_words(text):
     print(len(text.split()))
+
+def count_characters(text):
+    character_dict = {}
+    lower_text = text.lower()
+    for letter in lower_text:
+        # if ord(letter) >= 97 and ord(letter) <= 122:
+        if letter not in character_dict:
+            character_dict[letter] = 1
+        elif letter in character_dict:
+            character_dict[letter] += 1
+    print(character_dict)
 
 
 main()
